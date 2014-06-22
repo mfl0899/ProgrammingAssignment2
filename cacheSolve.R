210962232a390249e903ce7e44a80f980b4293cc
mfl0899 <mimi0899@gmail.com>
2014-06-22 18:00:53 -0400

    Check cached matrix
    Add code to check if cached matrix has been altered before using cached
    data

--- cacheSolve.R
+++ cacheSolve.R
@@ -3,14 +3,23 @@ cacheSolve <- function(x, ...) {
         ## inverse has already been computed and matrix has not changed, 
         ## then retrieve the inverse from  cache
         
-        inverseMatrix <- x$getinverse()
-        if(!is.na(inverseMatrix)) {
-                        message("getting cached data")
+        inverseMatrix <- x$getinverse() ## Check for cached matrix
+        
+        if(is.na(inverseMatrix)) {
+                        ## no cached matrix found - continue processing
+                else identical(x, inverseMatrix)
+                        ## cached matrix found; 
+                        ## verify cached matrix unchanged before using
+                        message("using cached data")
                         return(inverseMatrix)
+
+                ## changes found; continue to compute inverse
         }
-        data <- x$get()
-        inverseMatrix <- solve(data, ...)
-        x$setinverse(inverseMatrix)
+        
+        data <- x$get() 
+        inverseMatrix <- solve(data, ...) ## Compute inverse of matrix
+        x$setinverse(inverseMatrix)       ## Cache inverse matrix
+        
         inverseMatrix
         
 }
